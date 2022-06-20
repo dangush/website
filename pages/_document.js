dbdd6c30c91e580c598f0e0e0e551d5f3abd6b48
Daniel Gushchyan <d.gushchyan@gmail.com>
2022-06-19 22:20:20 -0700

    bugfix grr

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ class MyDocument extends Document {
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
             <link href="https://fonts.googleapis.com/css2?family=Courier+Prime&display=swap" rel="stylesheet"/>
             {/* <link href="../public/fonts/ocr-aregular.ttf" rel="stylesheet"/> */}
-            <link rel="icon" href="https://cdn.discordapp.com/attachments/364120933063065611/758916473186943006/411291026070700042.png">
+            <link rel="icon" href="https://cdn.discordapp.com/attachments/364120933063065611/758916473186943006/411291026070700042.png"/>
         </Head>
         <body>
           <Main />
